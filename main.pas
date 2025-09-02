f06699653987aa848e793f0b49a6ba3060018c3a
unknown <kboxkbox1234@gmail.com>
2025-09-02 13:00:33 +0200

    added logging into server

--- main.pas
+++ main.pas
@@ -5,16 +5,27 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, fphttpclient, opensslsockets, fpjson, jsonparser;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Menus,
+  fphttpclient, opensslsockets, fpjson, jsonparser;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    add_word: TButton;
+    login: TButton;
+    word_type: TComboBox;
+    email: TEdit;
+    password: TEdit;
+    new_word: TEdit;
     Generate_Joke: TButton;
+    procedure add_wordClick(Sender: TObject);
+    procedure loginClick(Sender: TObject);
+    procedure emailChange(Sender: TObject);
     procedure Generate_JokeClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure word_typeChange(Sender: TObject);
   private
 
   public
@@ -23,6 +34,8 @@ type
 
 var
   Form1: TForm1;
+
+  key:string;
 
 implementation
 
@@ -52,6 +65,122 @@ begin
 
 end;
 
+function sign_in(email: string; password: string): String;
+var
+   send:string;
+   Client: TFPHttpClient;
+   Response: TStringStream;
+   JsonData: TJSONData;
+   JsonObj: TJSONObject;
+
+begin
+   //prep string
+   send := '{"email" : "' + email + '", "password" : "' + password + '"}';
+
+   Client := TFPHttpClient.Create(nil);
+   Client.AddHeader('User-Agent', 'Mozilla/5.0 (compatible; fpweb)');
+   Client.AddHeader('Content-Type', 'application/json; charset=UTF-8');
+   Client.AddHeader('Accept', 'application/json');
+   Client.AllowRedirect := true;
+
+   Client.RequestBody := TRawByteStringStream.Create(send);
+   Response := TStringStream.Create('');
+
+   try
+    try
+      Client.Post('https://api.stanghelle.org/sign_in', Response);
+
+    except on E: Exception do
+      ShowMessage('Something bad happened: ' + E.Message);
+    end;
+  finally
+    Client.RequestBody.Free;
+    Client.Free;
+
+    JsonData := GetJSON(Response.DataString);
+    try
+      if JsonData.JSONType = jtObject then
+      begin
+        JsonObj := JsonData as TJSONObject;
+        key := JsonObj.Get('key', '');
+      end;
+    finally
+      JsonData.Free;
+    end;
+    result := key;
+    Response.Free;
+
+  end;
+end;
+
+function log_in(email: string; password: string): String;
+var
+   send:string;
+   Client: TFPHttpClient;
+   Response: TStringStream;
+   JsonData: TJSONData;
+   JsonObj: TJSONObject;
+
+begin
+   //prep string
+   send := '{"email" : "' + email + '", "password" : "' + password + '"}';
+
+   Client := TFPHttpClient.Create(nil);
+   Client.AddHeader('User-Agent', 'Mozilla/5.0 (compatible; fpweb)');
+   Client.AddHeader('Content-Type', 'application/json; charset=UTF-8');
+   Client.AddHeader('Accept', 'application/json');
+   Client.AllowRedirect := true;
+
+   Client.RequestBody := TRawByteStringStream.Create(send);
+   Response := TStringStream.Create('');
+
+   try
+    try
+      Client.Post('https://api.stanghelle.org/login', Response);
+
+    except on E: Exception do
+      ShowMessage('Something bad happened: ' + E.Message);
+    end;
+  finally
+    Client.RequestBody.Free;
+    Client.Free;
+
+    JsonData := GetJSON(Response.DataString);
+    try
+      if JsonData.JSONType = jtObject then
+      begin
+        JsonObj := JsonData as TJSONObject;
+        key := JsonObj.Get('key', '');
+      end;
+    finally
+      JsonData.Free;
+    end;
+    result := key;
+    Response.Free;
+
+  end;
+end;
+
+
+function authenticate(email:string; password:string):Boolean;
+var
+   test:string;
+begin
+     test := log_in(email, password);
+
+     if test = 'error' then
+        test := sign_in(email, password);
+
+     if test = 'error' then
+        result := false
+     else
+     begin
+         //key := test;
+         result := true;
+     end;
+end;
+
+
 { TForm1 }
 
 procedure TForm1.FormCreate(Sender: TObject);
@@ -59,9 +188,46 @@ begin
 
 end;
 
+procedure TForm1.word_typeChange(Sender: TObject);
+begin
+
+end;
+
 procedure TForm1.Generate_JokeClick(Sender: TObject);
 begin
      ShowMessage(get_joke);
+
+end;
+
+procedure TForm1.emailChange(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.add_wordClick(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.loginClick(Sender: TObject);
+var
+   email2:string;
+   password2:string;
+   auth_status:Boolean;
+
+begin
+
+     email2 := Self.email.Text;
+     password2 := Self.password.Text;
+
+     auth_status := authenticate(email2, password2);
+
+     if auth_status = true then
+        ShowMessage('authenticated good :)')
+     else
+         ShowMessage('authenticated bad >:(');
+
+
 end;
 
 end.
